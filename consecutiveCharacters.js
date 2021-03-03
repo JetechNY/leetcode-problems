58f0a72452a7008fd80781bffc21367924b95278
SunJet <60858671+JetechNY@users.noreply.github.com>
2021-03-03 10:00:27 -0500

    Update consecutiveCharacters.js

--- consecutiveCharacters.js
+++ consecutiveCharacters.js
@@ -35,38 +35,37 @@
 // Solution:
 
 var consecutiveCharacters = function(s) {
-   if (!s.length) return 0;
-    let count = 1,
+   if (!s.length) return 0;//checking for edge case if constraints werent in place
+    let count = 1,//since length is at least 1, setting count and max to 1 is ok. 
          max = 1
-  for (let i = 1; i < s.length; i++) {
-    if( s[i] == s[i-1]) {
-     max = Math.max(max, ++count) 
+  for (let i = 1; i < s.length; i++) { //starting at index 1 not 0. Allows me to look at previous instead of next.
+    if( s[i] == s[i-1]) { //compares current to previous value, if they are the same
+     max = Math.max(max, ++count) //increase count and compare to max, then set the bigger number to max
     }
     else{
-      count = 1
+      count = 1 //if previous val is not the same as current, or no repeat, then reset count to 1
     }
   }
-  return max
+  return max //return the max
 
 }
 
 
 // also works
 
-// var maxPower = function(s) {
-//     let count = 1,
-//          max = 1,
-//         previous = ' '
-//   for (let i = 0; i < s.length; i++) {
-//     let c = s.charAt(i)
-//     if( c == previous) {
-//       ++count
-//     }else{
-//       count = 1
-//       previous = c 
-//     }
-//     max = Math.max(max, count)
-//   }
-//   return max
-
-// }
+var maxPower = function(s) {
+    let count = 1,
+         max = 1,
+        previous = ' '//setting previous as empty string
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i)//c is set to the string char at i
+    if( c == previous) {// is current c the same as previous?
+      ++count//increase count if true
+    }else{
+      count = 1//otherwise reset count to 1
+      previous = c //set the previous to current
+    }
+    max = Math.max(max, count)//set max to the greater of the two, this always runs inside for loop
+  }
+  return max// when for loop complete, return max
+}
