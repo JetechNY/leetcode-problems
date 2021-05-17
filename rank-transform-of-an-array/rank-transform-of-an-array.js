73754b0b56b48962c3b9764fa69b8ca19c5c34c4
SunJet <60858671+JetechNY@users.noreply.github.com>
2021-05-17 12:39:22 -0400

    Time: 184 ms (95.38%), Space: 54.9 MB (95.38%) - LeetHub

--- rank-transform-of-an-array/rank-transform-of-an-array.js
+++ rank-transform-of-an-array/rank-transform-of-an-array.js
@@ -3,15 +3,16 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
-  let ranks = new Map()
-  let cloneArray = [...new Set(arr)]
-  
-  cloneArray.sort((a, b) => a - b)
-  cloneArray.forEach((n, i) => ranks.set(n, i))
-  
-  return arr.map(n => {
-    // using map.get(n) faster than cloneArray.indexOf(n) 99 times
-    //  and faster than use object 2 times
-    return ranks.get(n) + 1
-  })
+    const map = new Map();
+    // value -> rank 
+    
+    let rank = 1;
+    [...arr].sort((a,b) => a - b).forEach((number) => {
+        if (!map.has(number)) {
+            map.set(number, rank);
+            rank += 1;
+        }
+    })
+    
+    return arr.map(number => map.get(number));
 };
