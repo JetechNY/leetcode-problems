653ff132a46ebbd20a01539ee2ced60a45c6f1e5
SunJet <60858671+JetechNY@users.noreply.github.com>
2021-07-19 03:39:30 -1000

    Time: 116 ms (76.41%), Space: 45.5 MB (56.28%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,25 +3,24 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-  let arr=[]
+  let result = []
   let left = 0
   let right = nums.length -1
   
-  for(let i = nums.length -1 ; i >= 0  ;i--){
-    let square
-    if(Math.abs(nums[left]) < Math.abs(nums[right])){
-      square = nums[right]
-      right --
-    } else{
-      square = nums[left]
-      left++
-    }
-    arr[i] = square* square
-  } 
-  return arr
+  let insertIndex = nums.length -1
   
-  // for(let i = 0; i < nums.length ; i++){
-  //     arr[i] = nums[i] * nums[i]
-  //   }
-  // return arr.sort((a,b) => a-b) 
+  while(left <= right){
+    let num1 = nums[left]*nums[left]
+    let num2 = nums[right]*nums[right]
+    
+    if(num1 > num2){
+      result[insertIndex] = num1
+      left++
+    } else {
+      result[insertIndex] = num2
+      right--
+    }
+    insertIndex--
+  }
+  return result
 };
